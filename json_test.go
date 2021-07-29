c224b8604f0a958ecbcb8fa66644aff0d7c927d1
gilsuk <devgilsuk@gmail.com>
2021-07-29 21:11:25 +0900

    add test OverrideDefault but, now It fails

--- json_test.go
+++ json_test.go
@@ -30,3 +30,33 @@ func TestLoadJsonCompletly(t *testing.T) {
 		t.Fail()
 	}
 }
+
+func TestOverrideDefault(t *testing.T) {
+	config := &configuration{
+		Name: "Bob",
+	}
+
+	err := conf.LoadJson("./testdata/configuration.json", config)
+	if err != nil {
+		t.Errorf("fail when load file")
+	}
+
+	if config.Name != "Alice" || config.Age != 21 {
+		t.Fail()
+	}
+}
+
+func TestLoadDefault(t *testing.T) {
+	config := &configuration{
+		Name: "Bob",
+	}
+
+	err := conf.LoadJson("./testdata/configuration-with-default.json", config)
+	if err != nil {
+		t.Errorf("fail when load file")
+	}
+
+	if config.Name != "Bob" || config.Age != 21 {
+		t.Fail()
+	}
+}
